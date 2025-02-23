643ba6b714f3c6ddba2a75cc4926b39ef25e4206
manojLovesCoding <mkum9417@gmail.com>
2025-02-23 18:19:22 +0530

    commit-3

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import { BrowserRouter as Router, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
-//import Home from "./pages/Home";
-//import About from "./pages/About";
-//import Contact from "./pages/Contact";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
     <Router>
       <Layout>
         <Routes>
-          {/*<Route path="/" element={<Home />} />
+          <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />*/}
+          <Route path="/contact" element={<Contact />} />
         </Routes>
       </Layout>
     </Router>
