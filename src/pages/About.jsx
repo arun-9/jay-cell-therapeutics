a06e18aef7e1e08e08a99a65b980fad91b41e265
manojLovesCoding <mkum9417@gmail.com>
2025-03-05 10:25:49 +0530

    removeFewSection removed statistics section and testinomial section

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,7 +2,7 @@ import Breadcrumbs from "../components/AboutPageComponent/Breadcrumbs";
 import AboutSection from "../components/AboutPageComponent/AboutSection";
 //import StatisticsSection from "../components/AboutPageComponent/StatisticsSection";
 import TeamSection from "../components/AboutPageComponent/TeamMember";
-import TestimonialSlider from "../components/AboutPageComponent/TestimonialSlider";
+//import TestimonialSlider from "../components/AboutPageComponent/TestimonialSlider";
 import FAQAccordion from "../components/AboutPageComponent/FAQAccordion";
 import CTASection from "../components/AboutPageComponent/CTASection";
 const About = () => {
@@ -12,7 +12,7 @@ const About = () => {
       <AboutSection />
       {/*<StatisticsSection />*/}
       <TeamSection />
-      <TestimonialSlider />
+      {/*<TestimonialSlider />*/}
       <FAQAccordion />
       <CTASection />
     </div>
